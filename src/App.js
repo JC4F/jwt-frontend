44d518dd6613c5d707a21fb911aef34649536c66
JC4F <lequan12122002@gmail.com>
2022-10-16 23:37:14 +0700

    react router5 + design form login bstrp5

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.scss';
+import Nav from './components/Navigation/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Login from './components/Login/Login';
+
+function App() {
+  return (
+    <Router>
+      <div className='app-container'>
+
+        <Switch>
+          <Route path="/news">
+            news
+          </Route>
+          <Route path="/about">
+            about
+          </Route>
+          <Route path="/contact">
+            login
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/" exact>
+            home
+          </Route>
+          <Route path="*">
+            404 not found
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
