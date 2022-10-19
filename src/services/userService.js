30f6e812500700b24322a8f89618632596bc3b85
JC4F <lequan12122002@gmail.com>
2022-10-19 23:34:50 +0700

    1

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,4 +8,20 @@ const loginUser = (valueLogin, password)=>{
     return axios.post('http://localhost:8080/api/v1/login', {valueLogin, password})
 }
 
-export {registerNewUser, loginUser};
+const fetchAllUser = (page, limit)=>{
+    return axios.get(`http://localhost:8080/api/v1/user/read?page=${page}&limit=${limit}`)
+}
+
+const deleteUser = (user) => {
+    return axios.delete(`http://localhost:8080/api/v1/user/delete`, {data: {id: user.id}});
+};
+
+const fetchGroup = ()=>{
+    return axios.get(`http://localhost:8080/api/v1/group/read`)
+}
+
+const createNewUser = (userData)=>{
+    return axios.post('http://localhost:8080/api/v1/user/create', {...userData})
+}
+
+export {registerNewUser, loginUser, fetchAllUser, deleteUser, fetchGroup, createNewUser};
